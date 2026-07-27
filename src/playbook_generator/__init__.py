d7bf02ce093506786068024074f9b4bd89f9a42d
IBEH CHIGOZIEM <133787066+ThePreacherMan@users.noreply.github.com>
2026-07-27 12:31:01 +0100

    Configure playbook generator package

--- src/playbook_generator/__init__.py
+++ src/playbook_generator/__init__.py
@@ -1,1 +1,28 @@
+"""Incident Response Playbook Generator package."""
 
+from playbook_generator.generator import (
+    UnsupportedIncidentTypeError,
+    generate_playbook,
+    render_markdown,
+    save_playbook,
+)
+from playbook_generator.models import (
+    IncidentPlaybook,
+    IncidentSeverity,
+    ResponsePhase,
+)
+from playbook_generator.templates import get_supported_incident_types
+
+
+__all__ = [
+    "IncidentPlaybook",
+    "IncidentSeverity",
+    "ResponsePhase",
+    "UnsupportedIncidentTypeError",
+    "generate_playbook",
+    "get_supported_incident_types",
+    "render_markdown",
+    "save_playbook",
+]
+
+__version__ = "1.0.0"
